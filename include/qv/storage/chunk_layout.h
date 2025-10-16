2d04e6a62fdf27aaca52f68518a51a330c2b16a6
Dennis Östling <zilverztream@gmail.com>
2025-10-16 08:04:54 +0200

    TSK122_Weak_CRC32_for_Chunk_Headers: authenticate chunk headers

--- include/qv/storage/chunk_layout.h
+++ include/qv/storage/chunk_layout.h
@@ -24,10 +24,12 @@ struct ChunkHeader {
   uint8_t tag_size;
   uint8_t nonce_size;
   uint8_t reserved[5]{};
+  uint32_t integrity_version{0};        // TSK122_Weak_CRC32_for_Chunk_Headers versioned MAC binding
+  std::array<uint8_t, 32> header_mac{}; // TSK122_Weak_CRC32_for_Chunk_Headers dedicated MAC field
 };
 #pragma pack(pop)
 
-static_assert(sizeof(ChunkHeader) == 128, "ChunkHeader must be 128 bytes");
+static_assert(sizeof(ChunkHeader) == 160, "ChunkHeader must be 160 bytes"); // TSK122_Weak_CRC32_for_Chunk_Headers
 
 #pragma pack(push, 1)
 struct ChunkGroupHeader {
